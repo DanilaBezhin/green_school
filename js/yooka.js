57862f69eeefbef83833ba092b8adec657dc2a59
Danisimo <bezhin.2000@mail.ru>
2025-04-22 18:22:39 +0700

    add yookassa scripts

---
+++ js/yooka.js
@@ -0,0 +1,27 @@
+document.getElementById('payment-form').addEventListener('submit', async function (e) {
+    e.preventDefault(); // Предотвращаем обычное поведение формы
+
+    const email = document.getElementById('email').value;
+
+    try {
+        const response = await fetch('http://localhost:5000/pay', {
+            // Убедитесь, что адрес соответствует вашему URL
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Сервер вернул ошибку: ' + response.statusText);
+        }
+
+        const result = await response.json();
+        // Перенаправляем на страницу подтверждения оплаты
+        window.location.href = result.confirmation_url; 
+    } catch (error) {
+        console.error('Ошибка:', error);
+        alert('Произошла ошибка при оплате: ' + error.message);
+    }
+});
